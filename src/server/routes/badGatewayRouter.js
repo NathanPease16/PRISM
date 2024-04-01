6322717f8fc3e84d1016540713b27754aef5e53e
NathanPease16 <nspease@stu.naperville203.org>
2024-03-31 21:26:59 -0500

    Changed styling on bad gateway page to look a little bit better (doesn't need to be that good, just good enough)

--- src/server/routes/badGatewayRouter.js
+++ src/server/routes/badGatewayRouter.js
@@ -4,7 +4,14 @@ const route = express.Router();
 
 route.get('/badGateway', (req, res) => {
     res.status(404);
-    res.render('404', {originalUrl: req.query.old.substring(1)});
+    let originalUrl = 'unknown';
+    try {
+        originalUrl = req.query.old.substring(1);
+    } catch (err) {
+
+    }
+
+    res.render('404', {originalUrl});
 });
 
 route.get('*', (req, res) => {
