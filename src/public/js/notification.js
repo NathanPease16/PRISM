1f70931366de396af88a707a91463df2454523c0
Nathan Pease <nspease@stu.naperville203.org>
2024-04-12 14:14:13 -0500

    Finished commenting app.js and began working on commenting public files

--- src/public/js/notification.js
+++ src/public/js/notification.js
@@ -1,11 +1,21 @@
+/**
+ * Notifications that drop down from the top of the screen
+ */
 class Notification {
+    /**
+     * Constructs a new notification object
+     * @param {*} text The text to appear on the notification
+     * @param {*} color What color the notification should be (From a preset list red, green, blue, orange)
+     */
     constructor(text, color) {
         this.text = text;
         this.color = color;
 
+        // Create a new container to hold the notification
         const container = document.createElement('div');
         container.className = 'notification-container';
 
+        // Create the notification and assign its text and color
         const notification = document.createElement('div');
         notification.className = `notification notification-${this.color}`;
         notification.textContent = this.text;
